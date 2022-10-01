c989e4ffefc586bc6af2df5dc72fea73f9bd5976
KevinGuedes <kevinguedes@edu.unifor.br>
2022-09-30 21:24:34 -0300

    new task form completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Header } from './components/Header'
+import { NewTask } from './components/NewTask'
+
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.newTaskContainer}>
+        <NewTask />
+      </div>
     </div>
   )
 }
