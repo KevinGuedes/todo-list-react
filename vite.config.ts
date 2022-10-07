ccc6a5e57246360727a2907d7aaa22779009a933
KevinGuedes <kevinguedes@edu.unifor.br>
2022-10-06 21:11:40 -0300

    adding slashes to base config on vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'todo-list-react',
+  base: '/todo-list-react/',
   plugins: [react()],
   server: {
     open: true,
